4f4bc0c5a3dd3e568b064166de196c7423b6e254
Jean Carlo De Castro <jcdecastrop@hotmail.com>
2021-10-03 17:57:07 -0400

    Initial commit

---
+++ jc-dapper-extension/Attributes/DapperToolsAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace jc_dapper_extension.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class DapperToolsAttribute : System.Attribute
+    {
+        public bool Ignore { get; set; }
+    }
+}
---
+++ jc-dapper-extension/DapperExtension.cs
@@ -0,0 +1,145 @@
+﻿using Dapper;
+using System;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jc_dapper_extension
+{
+    public class DapperExtension
+    {
+        protected string TableName;
+        protected string PrimaryKeyField;
+        protected SqlConnection Connection;
+
+        private void _CommonValidations(object Record)
+        {
+            if (Record == null)
+                throw new ApplicationException("Record is required");
+        }
+
+        private bool _GetIgnoreAttributeProperty(PropertyInfo Property)
+        {
+            bool ignore_field = false;
+
+            //Check if column needs to be ignore
+            var dapper_tools_attribute = (Attributes.DapperToolsAttribute)Attribute.GetCustomAttribute(element: Property, attributeType: typeof(Attributes.DapperToolsAttribute));
+            if (dapper_tools_attribute != null)
+                ignore_field = dapper_tools_attribute.Ignore;
+
+            return ignore_field;
+        }
+
+        internal async Task<long> InsertAsync(object Record)
+        {
+            _CommonValidations(Record: Record);
+
+            //Get object properties
+            var properties = Record.GetType().GetProperties();
+
+            //Build sql statements
+            var SQL = new StringBuilder();
+            SQL.Append($@" INSERT INTO {TableName} (");
+
+            int count = 1;
+            foreach (var property in properties)
+            {
+                var ignore_field = _GetIgnoreAttributeProperty(Property: property);
+                if (ignore_field == false)
+                {
+                    if (count == properties.Count())
+                    {
+                        SQL.Append($"{property.Name})");
+                    }
+                    else
+                    {
+                        SQL.Append($"{property.Name},");
+                    }
+                }
+
+                count++;
+            }
+
+            SQL.Append(" VALUES (");
+
+            count = 1;
+            foreach (var property in properties)
+            {
+                var ignore_field = _GetIgnoreAttributeProperty(Property: property);
+                if (ignore_field == false)
+                {
+                    if (count == properties.Count())
+                    {
+                        SQL.Append($"@{property.Name})");
+                    }
+                    else
+                    {
+                        SQL.Append($"@{property.Name},");
+                    }
+                }
+
+                count++;
+            }
+
+            SQL.Append(" SELECT CAST(SCOPE_IDENTITY() as int)");
+
+            var retuned_value = await Connection.QuerySingleAsync<long>(sql: SQL.ToString(), param: Record);
+
+            return retuned_value;
+        }
+
+        internal async Task UpdateAsync(object Record)
+        {
+            _CommonValidations(Record: Record);
+
+            //Get object properties
+            var properties = Record.GetType().GetProperties();
+
+            //Build sql statements
+            var SQL = new StringBuilder();
+            SQL.Append($"UPDATE {TableName} SET ");
+
+            int count = 1;
+            foreach (var property in properties)
+            {
+                var ignore_field = _GetIgnoreAttributeProperty(Property: property);
+                if (ignore_field == false)
+                {
+                    if (count == properties.Count())
+                    {
+                        SQL.Append($"{property.Name} = @{property.Name}");
+                    }
+                    else
+                    {
+                        SQL.Append($"{property.Name} = @{property.Name},");
+                    }
+                }
+
+                count++;
+            }
+
+            SQL.Append($" WHERE {PrimaryKeyField} = @{PrimaryKeyField} ");
+
+            //Get primary key property
+            var primary_key_property = properties.FirstOrDefault(t => t.Name == PrimaryKeyField);
+            if (primary_key_property == null)
+                throw new ApplicationException("Invalid primary key field");
+
+            var primary_key_value = primary_key_property.GetValue(Record);
+
+            long.TryParse(primary_key_value?.ToString(), out long ID);
+
+            if (ID == 0)
+                throw new ApplicationException("Invalid primary key field");
+
+            await Connection.ExecuteAsync(sql: SQL.ToString(), param: Record);
+        }
+
+        internal async Task DeleteAsync(long ID)
+        {
+            await Connection.ExecuteAsync(sql: $" DELETE {TableName} WHERE {PrimaryKeyField} = @ID", param: new { ID });
+        }
+    }
+}
